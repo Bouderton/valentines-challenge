d0830e30566a2bd5f82d3bdc4c821b7fd837b778
bouderton <rmkhazal@gmail.com>
2024-02-09 21:04:53 -0800

    button event listener

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,18 @@
 const messageWrapper = document.querySelector(".message");
-const messageYesBtn = messageWrapper.querySelector("#yes-button");
+const yesBtn = messageWrapper.querySelector("#yes-button");
+let noBtn = messageWrapper.querySelector("#no-button");
+
+yesBtn.addEventListener("click", () => {
+  console.log("YIPPIE");
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
